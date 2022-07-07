9511a6c21132d8a4ce8711cc591beff201a84e79
jhonatanSantiago13 <jhonale44@hotmail.com>
2022-07-07 13:00:47 -0500

    fetch terminado

--- script.js
+++ script.js
@@ -16,6 +16,40 @@ Formulario.addEventListener('submit', (e) => {
 	let telefono     = datos.get("telefono");
 	let codigoPostal = datos.get("codigoPostal");
 
-	
+	/*console.log("nombre", nombre);
+	console.log("correo", correo);
+	console.log("telefono", telefono);
+	console.log("codigoPostal", codigoPostal);*/
+
+	const json_data = {
+
+		 "nombre":nombre,
+         "correo":correo,
+         "telefono":telefono,
+         "cp":codigoPostal
+
+	};
+
+	const url = "http://demo0176096.mockable.io/";
+
+	/*console.log("json", json_data);*/
+
+	fetch(url,{
+
+		method: "post",
+		body: json_data
+
+	})
+	.then(data => data.json())
+	.then(data =>{
+
+			console.log("resultado", data);
+
+	})
+	.catch(function(error){
+
+		console.error(error);
+
+	})
 
 })
